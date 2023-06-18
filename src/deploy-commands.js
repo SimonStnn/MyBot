2cb3a4f94984be459ae93ec825a0e8eb5ca7b795
SimonStnn <simon.stijnen.23@gmail.com>
2023-06-18 20:05:40 +0200

    Make script to deploy commands

---
+++ src/deploy-commands.js
@@ -0,0 +1,57 @@
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commands = [];
+// Grab all the command files from the commands directory you created earlier
+const foldersPath = path.join(__dirname,'..', 'build', 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    // Grab all the command files from the commands directory you created earlier
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs
+        .readdirSync(commandsPath)
+        .filter((file) => file.endsWith('.js'));
+    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        console.log(filePath);
+        try {
+            const command = require(filePath);
+            console.log("name:",command.default.data.name);
+            commands.push(command.default.data.toJSON());
+        } catch (err) {
+            console.log(err);
+            continue
+        }
+    }
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(
+    'ODE2NDMzODU0MzA5OTkwNDcy.YD65Rw.dE34cmKaQYoSiaDAKJxkoUZGW9Y'
+);
+
+// and deploy your commands!
+(async () => {
+    try {
+        console.log(
+            `Started refreshing ${commands.length} application (/) commands.`
+        );
+
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId),
+            { body: commands }
+        );
+
+        console.log(
+            `Successfully reloaded ${data.length} application (/) commands.`
+        );
+    } catch (error) {
+        // And of course, make sure you catch and log any errors!
+        console.error(error);
+    }
+})();
