4f83b71c53fa954beff75b637eb7e8bb86da87c7
SimonStnn <simon.stijnen.23@gmail.com>
2023-06-18 20:06:55 +0200

    Make command that deploys commands

---
+++ src/commands/moderation/deploy-commands.ts
@@ -0,0 +1,61 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction, Client } from 'discord.js';
+import Embed from '../../protocols/embed';
+import Command from '../../protocols/command';
+
+import { REST, Routes } from 'discord.js';
+import { clientId, guildId } from '../../config.json';
+import fs from 'node:fs';
+import path from 'node:path';
+
+export default new Command(
+    new SlashCommandBuilder()
+        .setName('deploy-commands')
+        .setDescription('Deploy commands!'),
+    async (client, interaction) => {
+        await interaction.deferReply();
+        const commands = [];
+        // Grab all the command files from the commands directory you created earlier
+        const foldersPath = path.join(__dirname, '..', '..', 'commands');
+        const commandFolders = fs.readdirSync(foldersPath);
+
+        for (const folder of commandFolders) {
+            // Grab all the command files from the commands directory you created earlier
+            const commandsPath = path.join(foldersPath, folder);
+            const commandFiles = fs
+                .readdirSync(commandsPath)
+                .filter((file) => file.endsWith('.js'));
+            // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+            for (const file of commandFiles) {
+                const filePath = path.join(commandsPath, file);
+                const command = require(filePath);
+                commands.push(command.default.data.toJSON());
+            }
+        }
+
+        // Construct and prepare an instance of the REST module
+        const rest = new REST().setToken(process.env.DISCORD_TOKEN as string);
+
+        // and deploy your commands!
+        try {
+            console.log(
+                `Started refreshing ${commands.length} application (/) commands.`
+            );
+
+            // The put method is used to fully refresh all commands in the guild with the current set
+            const data = await rest.put(
+                Routes.applicationGuildCommands(clientId, guildId),
+                { body: commands }
+            ) as Array<Object>;
+
+            console.log(
+                `Successfully reloaded ${data.length} application (/) commands.`
+            );
+            await interaction.editReply(new Embed({
+                content: `Successfully reloaded ${data.length} application (/) commands.`
+            }))
+        } catch (error) {
+            // And of course, make sure you catch and log any errors!
+            console.error(error);
+        }
+    },
+)
