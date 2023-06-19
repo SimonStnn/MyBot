aef99d5a0b3b4e96f6ff0a3c97bfee0100463e61
SimonStnn <simon.stijnen.23@gmail.com>
2023-06-19 13:22:18 +0200

    Add hook to pino that gets triggered every log

--- src/log/logger.ts
+++ src/log/logger.ts
@@ -1,10 +1,23 @@
 import { Client , Events} from 'discord.js';
 import pino from 'pino';
-const logger = pino();
 
+const logger = pino({
+    level: 'info',
+    hooks: {
+        logMethod(inputArgs, method, level) {
+            const logLevel = logger.levels.labels[level]
+            
+            if (inputArgs.length >= 2) {
+                const arg1 = inputArgs.shift()
+                const arg2 = inputArgs.shift()
+                return method.apply(this, [arg2, arg1, ...inputArgs])
+            }
+            return method.apply(this, inputArgs as [msg: string, ...args: any[]])
+        }
+    }
+});
 
 export function setupLogger(client: Client) {
-    logger.level = "info"
 
     client.on(Events.Debug, m => logger.debug(m));
     client.on(Events.Warn, m => {
