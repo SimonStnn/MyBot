3515fcd3bfdd2201d70fa54a16f5a84428651e1e
SimonStnn <simon.stijnen.23@gmail.com>
2023-06-19 10:46:43 +0200

    Construct commands with commandOptions

--- src/protocols/command.ts
+++ src/protocols/command.ts
@@ -1,15 +1,21 @@
-import { Client, Interaction, ChatInputCommandInteraction, SlashCommandBuilder, Message } from "discord.js";
+import { Client, Interaction, ChatInputCommandInteraction, SlashCommandBuilder, Message, CacheType } from "discord.js";
 
-export default class Command {
+interface CommandOptions {
     data: SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">
     execute: (client: Client, interaction: ChatInputCommandInteraction) => Promise<Response | any>
-    category: string | undefined;
+    category?: string;
+}
+
+export default class Command implements CommandOptions {
+    data: SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">;
+    execute: (client: Client<boolean>, interaction: ChatInputCommandInteraction<CacheType>) => Promise<Response | null>;
+    category?: string;
 
     constructor(
-        data: SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">,
-        execute: (client: Client, interaction: ChatInputCommandInteraction) => Promise<Response | any>
+        { data, execute, category }: CommandOptions
     ) {
         this.data = data
         this.execute = execute
+        this.category = category
     }
 }
