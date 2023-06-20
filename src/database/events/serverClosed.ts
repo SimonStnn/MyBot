216615fdb382e288574728a32cc25d3bbb09e308
SimonStnn <simon.stijnen.23@gmail.com>
2023-06-20 22:07:04 +0200

    Automatically reconnect to database

--- src/database/events/serverClosed.ts
+++ src/database/events/serverClosed.ts
@@ -1,8 +1,21 @@
 import { ActivityType, PresenceUpdateStatus } from "discord.js";
 import client from "../../client";
+import { connectToDatabase } from "../database";
+import logger from "../../log/logger";
+
+const wait = require('util').promisify(setTimeout);
 
 
 export default async function serverClosed() {
     client.user?.setStatus(PresenceUpdateStatus.DoNotDisturb)
     client.user?.setActivity('for a database connection.', { type: ActivityType.Watching });
+
+    await wait(60000)
+
+    try {
+        await connectToDatabase()
+        logger.warn("Succesfully reconnected to database!")
+    } catch (error) {
+        logger.error(error, "Faired to reconnect to database.")
+    }
 }
