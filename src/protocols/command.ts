e9356c96e81dd74dd15fc0790bb9696254aeaecf
SimonStnn <simon.stijnen.23@gmail.com>
2023-06-26 18:59:42 +0200

    Add autocomplete handler for commands

--- src/protocols/command.ts
+++ src/protocols/command.ts
@@ -1,9 +1,12 @@
-import { Client, ChatInputCommandInteraction, SlashCommandBuilder, CacheType } from "discord.js";
+import { Client, ChatInputCommandInteraction, SlashCommandBuilder, CacheType, Events } from "discord.js";
+import client from "../client";
+import logger from "../log/logger";
 
 interface CommandOptions {
     data: SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">
     execute: (client: Client, interaction: ChatInputCommandInteraction) => Promise<Response | any>
     category?: string;
+    autocompleteChoices?: string[]
 }
 
 export default class Command implements CommandOptions {
@@ -12,10 +15,32 @@ export default class Command implements CommandOptions {
     category?: string;
 
     constructor(
-        { data, execute, category }: CommandOptions
+        { data, execute, category, autocompleteChoices }: CommandOptions
     ) {
         this.data = data
         this.execute = execute
         this.category = category
+
+        if (autocompleteChoices) {
+            client.on(Events.InteractionCreate, async (interaction) => {
+                if (
+                    !interaction.isAutocomplete() ||
+                    interaction.commandName !== this.data.name
+                ) return
+
+                const focusedValue = interaction.options.getFocused();
+                let filtered = autocompleteChoices
+                    .filter((choice) => choice.startsWith(focusedValue))
+                    .slice(0, 25);
+
+                try {
+                    await interaction.respond(
+                        filtered.map((choice) => ({ name: choice, value: choice }))
+                    );
+                } catch (err) {
+                    logger.error(err)
+                }
+            });
+        }
     }
 }
