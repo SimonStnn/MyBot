1c273366de43953ce5cd8a8b42e043b9112f4d62
SimonStnn <simon.stijnen.23@gmail.com>
2023-06-30 23:29:13 +0200

    Remove unused wait func in userinfo

--- src/commands/utility/userinfo.ts
+++ src/commands/utility/userinfo.ts
@@ -2,8 +2,6 @@ import { SlashCommandBuilder } from 'discord.js';
 import Command from '../../protocols/command';
 import Response from '../../protocols/response';
 
-const wait = require('util').promisify(setTimeout);
-
 export default new Command({
     data: new SlashCommandBuilder()
         .setName('userinfo')
