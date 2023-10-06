b7959bd797d04f2cac5b8b998a0ae0b5b36d850f
SimonStnn <simon.stijnen.23@gmail.com>
2023-10-06 22:20:14 +0200

    Make goodnight schedule

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,43 @@
 // Require the necessary discord.js classes
-import fs from 'node:fs';
-import path from 'node:path';
-import { Collection } from 'discord.js';
-import { MongoClient, ServerApiVersion } from 'mongodb';
-require('dotenv').config();
-import Command from './protocols/command';
-import client from './client'
-import logger from './log/logger';
+import fs from "node:fs";
+import path from "node:path";
+import { Collection } from "discord.js";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import cron from "node-cron";
+require("dotenv").config();
+import Command from "./protocols/command";
+import client from "./client";
+import logger from "./log/logger";
 
 //
 //* Init database
 //
 
 client.dbClient = new MongoClient(process.env.DATABASE_CONNECTION as string, {
-   serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-   }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 //
 //* Event handler
 //
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of eventFiles) {
-   const filePath = path.join(eventsPath, file);
-   const event = require(filePath).default;
-   if (event.once) {
-      client.once(event.name, (...args) => event.execute(client, ...args));
-   } else {
-      client.on(event.name, (...args) => event.execute(client, ...args));
-   }
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath).default;
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(client, ...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(client, ...args));
+  }
 }
 
 //
@@ -42,26 +45,44 @@ for (const file of eventFiles) {
 //
 
 client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandSubFolders = fs.readdirSync(commandsPath).filter(file => {
-   const filePath = path.join(commandsPath, file);
-   return fs.statSync(filePath).isDirectory();
+const commandsPath = path.join(__dirname, "commands");
+const commandSubFolders = fs.readdirSync(commandsPath).filter((file) => {
+  const filePath = path.join(commandsPath, file);
+  return fs.statSync(filePath).isDirectory();
 });
 for (const folder of commandSubFolders) {
-   const subfolderPath = path.join(commandsPath, folder)
-   const commandFiles = fs.readdirSync(subfolderPath).filter(file => file.endsWith('.js'));
-   for (const file of commandFiles) {
-      const filePath = path.join(subfolderPath, file);
-      const command: Command = require(filePath).default;
-      command.category = folder
+  const subfolderPath = path.join(commandsPath, folder);
+  const commandFiles = fs
+    .readdirSync(subfolderPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(subfolderPath, file);
+    const command: Command = require(filePath).default;
+    command.category = folder;
 
-      // Set a new item in the Collection with the key as the command name and the value as the exported module
-      if (command.data) {
-         client.commands.set(command.data.name, command);
-      } else {
-         logger.warn(`[WARNING] The command at ${filePath} is missing a required "data" property.`);
-      }
-   }
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if (command.data) {
+      client.commands.set(command.data.name, command);
+    } else {
+      logger.warn(
+        `[WARNING] The command at ${filePath} is missing a required "data" property.`
+      );
+    }
+  }
+}
+
+//
+//* Cron schedule handler
+//
+
+const scheduleFiles = fs
+  .readdirSync(path.join(__dirname, "schedules"))
+  .filter((file) => file.endsWith(".js"));
+for (const file of scheduleFiles) {
+  const schedule = require(path.join(__dirname, "schedules", file)).default;
+  cron.schedule(schedule.time, () => {
+    schedule.execute(client);
+  });
 }
 
 export default client;
---
+++ src/schedules/goodnight.ts
@@ -0,0 +1,69 @@
+import { Client } from "discord.js";
+import { userIds, colors } from "../config.json";
+import logger from "../log/logger";
+import Embed from "../protocols/embed";
+
+const aansprekingen = [
+  "Hey",
+  "Hallo",
+  "Yo",
+  "Dear",
+  "Geachte",
+  "",
+  "Sup",
+  "Liefste",
+];
+
+const messages = [
+  "ik wens je een zeer fijne nacht. Dus ga maar naar je bedje 🛏, doe je oogjes toe en droom 💭 over de mooiste dingen. Slaapwel xoxo",
+  "hopelijk heb je zoete dromen vannacht en word je morgen fris als een vis wakker. 🐟",
+  "goede nacht. Droom mooie dromen vanavond. 🌙",
+  "de sterren zien er vanavond zo mooi uit, misschien kan je ze vanacht eens een bezoekje brengen. ✨",
+  "ik hoop dat je een zalige dag hebt gehad, maar nu is het tijd om een dutje te doen. Je nachtrust is belangrijk. 👆",
+  "slaapwel. 😴",
+  "sl🐒wel.",
+  "slaapwel, tot morgen. Juw!",
+  "het voelt zo goed dat ik jou heb leren kennen. Ik wilde je een fijne nachtrust wensen en je laten weten dat ik aan je denk. Sweet dreams. 💤",
+  "een hele fijne nacht leukerd. Tot in onze dromen! 💭",
+  "hele zoete dromen deze nacht!",
+  "als ik je een goede nacht wens, wens me me dan niet terug, omdat ik niet wil slapen. Ik zal de hele nacht aan je denken.",
+  "slaapwel, Ik kan niet wachten om wakker te worden en je weer te zien 👀, Ik breng liever met jou de nacht door 😊 maar het zal alleen moeten helaas 😔, tot morgen.",
+  "het wordt al laat, ik ga schaapjes 🐑 tellen, slaapwel!",
+  "ik zal aan je denken vannacht, slaapwel. <3",
+  "ik wens u een goede nachtrust en dat u zoete dromen hebt, Goede nacht.",
+  "ik houd zo veel van jou, ik ben zo blij dat we elkaar ontmoet hebben, wat alweer zo lang geleden lijkt. 🕰",
+];
+
+// ┌────────────── second
+// │ ┌──────────── minute
+// │ │ ┌────────── hour
+// │ │ │ ┌──────── day of month
+// │ │ │ │ ┌────── month
+// │ │ │ │ │ ┌──── day of week
+// │ │ │ │ │ │
+// * * * * * *
+export default {
+//   time: "10 22 * * *",
+  time: "0 10 22 * * *",
+  async execute(client: Client) {
+    // Make embed
+    const embed = new Embed({ title: "Bedtijd!" })
+      .setColor(`#${colors.pink.replace("#", "")}`)
+      .setTimestamp();
+    // Get aanspreking and random gn message from array
+    const aanspreking =
+      aansprekingen[Math.floor(Math.random() * aansprekingen.length)];
+    const message = messages[Math.floor(Math.random() * messages.length)];
+
+    const users = Object.values(userIds);
+    // Send embed to users.
+    for (const user of users) {
+      embed.setDescription(`${aanspreking} <@${user}>, ` + message);
+
+      const spamPerson = await client.users.fetch(user);
+      await spamPerson.send({ embeds: [embed] }).catch((err) => {
+        logger.error(err, `Failed to send good night message to ${user}`);
+      });
+    }
+  },
+};
